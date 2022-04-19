979011b0a37257446b3e64c05911b1693a4847f6
Aderiye <69057307+adexy-ai@users.noreply.github.com>
2022-04-19 21:51:25 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+function whosPaying(names) {
+    selectedNames = Math.floor(Math.random() * names.length) + 1;
+    return names[selectedNames];
+    }
+console.log(whosPaying(names));
+ 
+myName = 'ade';
+console.log(myName);
