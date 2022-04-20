c837e86a7771dd8dddf3646600b3edab245d2520
Aderiye <69057307+adexy-ai@users.noreply.github.com>
2022-04-20 13:05:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ console.log(whosPaying(names));
  
 myName = 'ade';
 console.log(myName);
+
+let fibNum = [];
+let n = fibNum
+function fibonacciGenerator(n) {
+    //Do NOT change any of the code above 👆
+    
+    //Write your code here:
+    for (let i = 0; i <= n; i++) {
+        n = [n.push(fibNum)]; 
+        return n
+        //if ()
+    }
+    console.log(fibonacciGenerator(5));
+}
